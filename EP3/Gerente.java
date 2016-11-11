a225f82e4c2b9ba3d253fedffc1082cd49ead6de
Nicolas Nogueira Lopes da Silva <nicolas.nogueira.silva@usp.br>
2016-11-11 14:34:26 -0200

    base leitura

---
+++ EP3/Gerente.java
@@ -0,0 +1,81 @@
+/******************************************************************************
+ *  Nome:   Bruno Rafael Arico                  8125459
+ *          Nicolas Nogueira Lopes da Silva     9277541
+ *
+ *****************************************************************************/
+
+public class Gerente {
+
+    int total;
+    int virtual;
+    int s;
+    int p;
+    Queue<Processo> fila;
+
+    public class Processo {
+        double t0;
+        double tf;
+        String nome;
+        int b;
+        int pid;
+        int[] posacessos;
+        double[] tempacessos;
+
+        public Processo (String[] strargs, int pidt) {
+            t0 = Double.parseDouble(strargs[0]);
+            nome = strargs[1];
+            tf = Double.parseDouble(strargs[2]);
+            b = Integer.parseInt(strargs[3]);
+            pid = pidt;
+            int size = (strargs.length - 4)/2;
+            posacessos = new int[size];
+            tempacessos = new double[size];
+            for (int i = 4, j = 0; i < strargs.length; i = i + 2, j++) {
+                posacessos[j] = Integer.parseInt(strargs[i]);
+                tempacessos[j] = Double.parseDouble(strargs[i]);
+            }
+        }
+
+    }
+
+    public void carregarArquivo (String arquivo) {
+        String str;
+        int pidt = 1;
+        In in = new In(arquivo);
+        total = in.readInt();
+        virtual = in.readInt();
+        s = in.readInt();
+        p = in.readInt();
+        fila = new LinkedList<Processo>();
+        while (!in.isEmpty()) {
+            str = in.readLine();
+            String sargs[] = str.split(" ");
+            fila.add(new Processo(sargs, pidt));
+            pidt++;
+        }
+    }
+
+
+    // test client
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente();
+        while (true) {
+            System.out.println("(ep3): ");
+            String comando = StdIn.readString();
+            if (comando.equals("carrega")) {
+                carregarArquivo (StdIn.readString());
+            } else if (comando.equals("espaco")) {
+
+            } else if (comando.equals("substitui")) {
+
+            } else if (comando.equals("executa")) {
+
+            } else if (comando.equals("sai")) {
+                System.out.println("");
+                break;
+            } else {
+                System.out.println("Comando desconhecido\n")
+            }
+        }
+    }
+}
