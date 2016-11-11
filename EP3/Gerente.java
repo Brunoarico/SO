dab2495a1539ca786c5b89b465030d88c15f2415
Nicolas Nogueira Lopes da Silva <nicolas.nogueira.silva@usp.br>
2016-11-11 14:42:35 -0200

    interface

--- EP3/Gerente.java
+++ EP3/Gerente.java
@@ -10,6 +10,9 @@ public class Gerente {
     int virtual;
     int s;
     int p;
+    int algespaco;
+    int algsubstitui;
+    double dt;
     Queue<Processo> fila;
 
     public class Processo {
@@ -63,13 +66,13 @@ public class Gerente {
             System.out.println("(ep3): ");
             String comando = StdIn.readString();
             if (comando.equals("carrega")) {
-                carregarArquivo (StdIn.readString());
+                gerente.carregarArquivo (StdIn.readString());
             } else if (comando.equals("espaco")) {
-
+                gerente.algespaco = StdIn.readInt();
             } else if (comando.equals("substitui")) {
-
+                gerente.algsubstitui = StdIn.readInt();
             } else if (comando.equals("executa")) {
-
+                gerente.dt = StdIn.readDouble();
             } else if (comando.equals("sai")) {
                 System.out.println("");
                 break;
